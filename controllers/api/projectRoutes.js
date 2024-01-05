946ced4676954da3dbac5618e4ccf5db52deac2a
amaddatu <>
2024-01-04 20:43:27 -0500

    Remove package-lock

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const { Project } = require('../../models');
 const withAuth = require('../../utils/auth');
+// // helps read maps_api_key
+// require('dotenv').config();
 
 router.post('/', withAuth, async (req, res) => {
+  // // reading maps_api_key
+  // if(process.env.MAPS_API_KEY){
+  //   console.log("I have the api key for maps");
+  // }
   try {
     const newProject = await Project.create({
       ...req.body,
